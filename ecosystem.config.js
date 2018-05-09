61f679ba5ab1c00ac63d59a73f759a9c7f71b5f8
Tony Jiang <white87332@gmail.com>
2018-05-09 10:11:18 +0800

    add ecosystem.js

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+    apps: [
+        {
+            name: 'iSuntvLive',
+            script: 'build/server.js',
+            log_date_format: 'YYYY-MM-DD',
+            watch: [
+                'build', 'public', 'locales'
+            ],
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ],
+
+    /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            key: `${process.env.HOME}/Documents/ssh/chinaiSun.pem`,
+            host: '54.65.45.58',
+            ref: 'origin/master',
+            repo: 'https://white87332@bitbucket.org/tidenet/isuntvlive.git',
+            path: '/home/ubuntu/workspace/isuntvlive',
+            'post-deploy': 'npm i && pm2 reload /home/ubuntu/workspace/isuntvlive/ecosystem.config.js',
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    }
+};
